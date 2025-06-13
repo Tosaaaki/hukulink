61f9aeb23bddc22ea057d8bac7543410a9ff7cfb
Tosaaaki <www.tosakiweb.net@gmail.com>
2025-06-14 01:06:37 +0900

    Use env var for SECRET_KEY and add example

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m(=$%1n!_spfk^$th%if*&hq0mb0&3mab)5h$+xotj-_1(k7zy"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
